83b4de32e18ab1a08bcaf81f6c08c386167c0d4d
AyushAnkit15 <593ayush.2020@gmail.com>
2024-02-20 15:37:10 +0530

    cart added

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,17 @@
 import { Outlet ,Link} from "react-router-dom";
 import { Fragment , useContext } from "react";
 import Logo from '../../assests/007 crown.svg'
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.contexts";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import { CartContext } from "../../contexts/cart.context";
+
 
 import './navigation.styles.scss'
 const Navigation = () => {
     const {currentUser , setCurrentUser} = useContext(UserContext)
+    const {isCartOpen} = useContext(CartContext)
 
 //     const signOutHandler = async()=>{
 // const response = await signOutUser() ; 
@@ -37,8 +42,13 @@ const Navigation = () => {
                 </Link>)
                 
             }
+            <CartIcon/>
            
         </div>
+        {
+            isCartOpen &&     <CartDropdown/>
+        }
+    
         </div>  
         <Outlet/>
     </Fragment>
